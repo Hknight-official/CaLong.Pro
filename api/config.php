6d6f0b27e40891a18e7667fd0379fb4d90c4acd9
Thà Lê <43161363+Hknight-official@users.noreply.github.com>
2020-07-30 16:03:04 +0700

    Update config.php

--- api/config.php
+++ api/config.php
@@ -6,7 +6,7 @@
 	$local_db = "localhost";
 	$user_db = "root";
 	$pass_db = "";
-	$name_db = "api_thesieutoc";
+	$name_db = "api_calong";
 	# đừng đụng vào 
   $conn = new mysqli($local_db, $user_db, $pass_db, $name_db);
   $conn->set_charset("utf8");
